6fcaf3fd1d699bbb888616b57e0c0fe65757828b
svensktutby <svensk.tut.by@gmail.com>
2021-04-03 14:37:22 +0300

    feat: add api instance

---
+++ src/main/dal/api.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import { DEV } from '../../config';
+
+export const baseURL = DEV
+  ? 'http://localhost:7542/2.0/'
+  : 'https://neko-back.herokuapp.com/2.0/';
+
+export const api = axios.create({
+  baseURL,
+  withCredentials: true,
+});
