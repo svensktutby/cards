3c3611853413acfb319435aad5ccfd5c443cae8d
Natalia Danilchanka <nataladanilcenko@gmail.com>
2021-04-06 11:23:18 +0300

    feat: add RegistrationPage files

--- src/pages/registration/ui/RegistrationPage.tsx
+++ src/pages/registration/ui/RegistrationPage.tsx
@@ -1,11 +1,42 @@
-import React, { FC } from 'react';
+import React, {FC, useState} from 'react';
 
 import s from './RegistrationPage.module.scss';
+import {InputText} from "../../../common/ui/InputText";
+import {Button} from "../../../common/ui/Button";
+import {PATH} from "../../../main/ui/App/Routes";
+import {Link} from "react-router-dom";
+
+
+
+
 
 export const RegistrationPage: FC = () => {
-  return (
-    <section className={s.page}>
-      <h2>Registration</h2>
-    </section>
-  );
+
+    return (
+        <section className={s.page}>
+            <h2>Registration</h2>
+            <div>
+                <InputText
+                    placeholder={'email'}
+                    type={'email'}
+                />
+            </div>
+            <div>
+                <InputText placeholder={'password'}
+                           type={'password'}
+                />
+            </div>
+            <div>
+                <InputText placeholder={'repeat password'}
+                           type={'password'}
+                />
+            </div>
+            <div className={s.block}><Button>Sing Up</Button></div>
+            <div className={s.block}>
+                <Link to={PATH.LOGIN} >
+                    Login
+                </Link>
+            </div>
+        </section>
+    );
 };
