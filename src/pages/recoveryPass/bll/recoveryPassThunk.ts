db0819dbc7b85234629b535d6c445ab841946ec5
svensktutby <svensk.tut.by@gmail.com>
2021-04-06 19:20:43 +0300

    feat: create state, actions, tests for recoveryPass reducer

---
+++ src/pages/recoveryPass/bll/recoveryPassThunk.ts
@@ -0,0 +1,6 @@
+import { ThunkType } from '../../../main/bll/store';
+import { RecoveryPassActionsType } from './recoveryPassActions';
+
+export const loginTC = (): ThunkType<RecoveryPassActionsType> => async (
+  dispatch,
+) => {};
