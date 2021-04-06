522a9f70dabb241c40f7c7582b146e51c3da983f
Natalia Danilchanka <nataladanilcenko@gmail.com>
2021-04-06 14:22:38 +0300

    feat: create form for RegistrationPage

--- src/pages/registration/ui/RegistrationPage.tsx
+++ src/pages/registration/ui/RegistrationPage.tsx
@@ -1,10 +1,7 @@
-import React, {FC, useState} from 'react';
+import React, {FC} from 'react';
 
 import s from './RegistrationPage.module.scss';
-import {InputText} from "../../../common/ui/InputText";
-import {Button} from "../../../common/ui/Button";
-import {PATH} from "../../../main/ui/App/Routes";
-import {Link} from "react-router-dom";
+import {RegistrationFormContainer} from "./RegistrationForm/RegistrationFormContainer";
 
 
 
@@ -15,28 +12,9 @@ export const RegistrationPage: FC = () => {
     return (
         <section className={s.page}>
             <h2>Registration</h2>
-            <div>
-                <InputText
-                    placeholder={'email'}
-                    type={'email'}
-                />
-            </div>
-            <div>
-                <InputText placeholder={'password'}
-                           type={'password'}
-                />
-            </div>
-            <div>
-                <InputText placeholder={'repeat password'}
-                           type={'password'}
-                />
-            </div>
-            <div className={s.block}><Button>Sing Up</Button></div>
-            <div className={s.block}>
-                <Link to={PATH.LOGIN} >
-                    Login
-                </Link>
-            </div>
+
+            <RegistrationFormContainer/>
+
         </section>
     );
 };
