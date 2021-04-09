cec80a77ed27bc4300ad2ca6d44e6488357a2360
Natalia Danilchanka <nataladanilcenko@gmail.com>
2021-04-09 09:30:06 +0300

    fix: RegistrationForm, RegistrationFormContainer

--- src/pages/registration/ui/RegistrationForm/RegistrationForm.tsx
+++ src/pages/registration/ui/RegistrationForm/RegistrationForm.tsx
@@ -1,4 +1,4 @@
-import React, {FC, FormEvent} from 'react';
+import React, {ChangeEvent, FC, FormEvent} from 'react';
 import { Link } from 'react-router-dom';
 
 import s from './RegistrationForm.module.scss'
@@ -8,10 +8,22 @@ import {LoginLinkType} from "./RegistrationFormContainer";
 
 
 type PropsType = {
+    email: string;
+    setEmail: (email: ChangeEvent<HTMLInputElement>) => void;
+    password: string;
+    setPassword: (password: ChangeEvent<HTMLInputElement>) => void;
+    repeatPass: string;
+    setRepeatPass: (repeatPass:  ChangeEvent<HTMLInputElement>) => void;
+
     loginLink: LoginLinkType;
 };
 
-export const RegistrationForm: FC<PropsType> = ({loginLink: { link, title }}) => {
+export const RegistrationForm: FC<PropsType> = ({
+                                                    loginLink: { link, title },
+                                                    email, setEmail,
+                                                    password, setPassword,
+                                                    repeatPass, setRepeatPass,
+}) => {
     const submitHandler = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log('submit');
@@ -19,15 +31,28 @@ export const RegistrationForm: FC<PropsType> = ({loginLink: { link, title }}) =>
 
     return (
         <form className={s.form} onSubmit={submitHandler}>
-            <InputText type="email" placeholder="Email" />
-            <InputText type="password" placeholder="Password" />
-            <InputText type="password" placeholder="Repeat password"/>
+            <InputText type="email"
+                       placeholder="Email"
+                       value={email}
+                       onChange={setEmail}
+            />
+            <InputText type="password"
+                       placeholder="Password"
+                       value={password}
+                       onChange={setPassword}
+            />
+            <InputText type="password"
+                       placeholder="Repeat password"
+                       value={repeatPass}
+                       onChange={setRepeatPass}
+            />
 
             <Button type="submit" >Sing Up</Button>
 
             <Link to={link} className={s.link}>
                 {title}
             </Link>
+
         </form>
     );
 };
--- src/pages/registration/ui/RegistrationForm/RegistrationFormContainer.tsx
+++ src/pages/registration/ui/RegistrationForm/RegistrationFormContainer.tsx
@@ -1,25 +1,55 @@
-import React, { FC } from 'react';
+import React, {ChangeEvent, FC, useCallback, useState} from 'react';
 
 import {
-  capitalizeFirstLetter,
-  transformLinkToTitle,
+    capitalizeFirstLetter,
+    transformLinkToTitle,
 } from '../../../../utils/textTransform';
-import { PATH } from '../../../../main/ui/App/Routes';
+import {PATH} from '../../../../main/ui/App/Routes';
 import {RegistrationForm} from "./RegistrationForm";
+import {useDispatch} from "react-redux";
+
 
 
 export const RegistrationFormContainer: FC = () => {
-  const { LOGIN } = PATH;
+    const {LOGIN} = PATH;
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [repeatPass, setRepeatPass] = useState<string>('');
 
-  const loginLink: LoginLinkType = {
-    link: LOGIN,
-    title: capitalizeFirstLetter(transformLinkToTitle(LOGIN)),
-  };
+    const setEmailCallback = useCallback(
+        (e: ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value),
+        [setEmail]
+    );
+    const setPasswordCallback = useCallback(
+        (e: ChangeEvent<HTMLInputElement>) => setPassword(e.currentTarget.value),
+        [setPassword]
+    );
+    const setRepeatPassCallback = useCallback(
+        (e: ChangeEvent<HTMLInputElement>) => setRepeatPass(e.currentTarget.value),
+        [setRepeatPass]
+    )
+    const dispatch = useDispatch();
 
-  return <RegistrationForm loginLink={loginLink} />;
+
+    const loginLink: LoginLinkType = {
+        link: LOGIN,
+        title: capitalizeFirstLetter(transformLinkToTitle(LOGIN)),
+    };
+
+    return <RegistrationForm
+        email={email} setEmail={setEmailCallback}
+        password={password} setPassword={setPasswordCallback}
+        repeatPass={repeatPass} setRepeatPass={setRepeatPassCallback}
+        loginLink={loginLink}/>;
 };
 
 export type LoginLinkType = {
-  link: string;
-  title: string;
+    link: string;
+    title: string;
 };
+
+export type DataRegistrationType = {
+    email: string,
+    password: string,
+    repeatPass: string;
+}
